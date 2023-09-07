696a6fee643adc51a21ada31ed449cfa6673a3c7
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2023-09-07 19:09:11 -0300

    feat: Insere na planilha base servidores que estavam na planilha nova mas nao na planilha base

--- src/utils_layer/utils.py
+++ src/utils_layer/utils.py
@@ -8,11 +8,35 @@ class Utils:
 
     def validate_file_exist(self, file_name: str) -> bool:
         """Valida se o arquivo da planilha existe"""
+
         if os.path.isfile(f"./{file_name}"):
             return True
         return False
 
     def xlsx_to_df(self, file_name: str) -> pd.DataFrame:
         """Transforma o arquivo xlsx em um dataframe do pandas"""
+
         planilha_df = pd.read_excel(f"./{file_name}")
+        return planilha_df
+
+    def xlsx_to_df_base_spreadsheet(self, file_name: str) -> pd.DataFrame:
+        """Transforma o arquivo de planilha base xlsx em um dataframe do pandas"""
+
+        planilha_df = pd.read_excel(
+            f"./{file_name}",
+            converters={
+                "JAN": str,
+                "FEV": str,
+                "MAR": str,
+                "ABR": str,
+                "MAI": str,
+                "JUN": str,
+                "JUL": str,
+                "AGO": str,
+                "SET": str,
+                "OUT": str,
+                "NOV": str,
+                "DEZ": str,
+            },
+        )
         return planilha_df
